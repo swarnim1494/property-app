732bed667e65efd4a558a494a7717e291462402f
swarnimshrivastava <swarnim1494@gmail.com>
2020-01-07 10:53:47 +0530

    fixed - counterparty signature gathering

--- workflows/src/main/java/com/template/flows/Responder.java
+++ workflows/src/main/java/com/template/flows/Responder.java
@@ -8,6 +8,8 @@ import net.corda.core.flows.*;
 import net.corda.core.transactions.SignedTransaction;
 import org.jetbrains.annotations.NotNull;
 
+import static net.corda.core.contracts.ContractsDSL.requireThat;
+
 // ******************
 // * Responder flow *
 // ******************
@@ -34,6 +36,22 @@ public class Responder extends FlowLogic<Void> {
             counterpartySession.send(false);
         }
 
+        class SignTxFlow extends SignTransactionFlow {
+            private SignTxFlow(FlowSession otherPartySession) {
+                super(otherPartySession);
+            }
+
+            @Override
+            protected void checkTransaction(SignedTransaction stx) {
+                requireThat(require -> {
+
+                    return null;
+                });
+            }
+        }
+
+        SecureHash expectedTxId = subFlow(new SignTxFlow(counterpartySession)).getId();
+
         subFlow(new ReceiveFinalityFlow(counterpartySession));
 
         return null;
@@ -41,7 +59,7 @@ public class Responder extends FlowLogic<Void> {
 }
 
 /*
-flow start Initiator propertyID: 1, address: "Happy Lane, Bellundur, Blr", surveyorApproved: false, owner: "GOI", surveyor: "Surveyor", buyer: null
+flow start Initiator propertyID: 1, address: "Happy Lane, Bellundur, Blr", owner: "GOI", surveyor: "Surveyor"
 
 run vaultQuery contractStateType: com.template.states.TemplateState
  */
